e8ccf4ce13a827e7d57e1c5b2ab46d6836c0579e
VaishnavYash <y01vaishnava@gmail.com>
2025-12-20 22:58:42 +0530

    Intital Commit

---
+++ lib/view/pdf_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class PDFUploadPage extends StatefulWidget {
+  const PDFUploadPage({super.key});
+
+  @override
+  State<PDFUploadPage> createState() => _PDFUploadPageState();
+}
+
+class _PDFUploadPageState extends State<PDFUploadPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text('PDF Upload Page')
+      ],
+    );
+  }
+}
